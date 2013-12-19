dc8270b2127331f0807b3ffc198229512e080a83
alex@laptop <karasev.alexey@gmail.com>
2013-12-20 01:39:16 +0400

    added daemon

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'rspec'
 gem 'rest-client'
+gem 'daemons'
--- lib/IO/daemon.rb
+++ lib/IO/daemon.rb
@@ -1,10 +1,15 @@
 require 'daemons'
+require_relative 'web'
+require_relative 'lan'
 
-Daemons.run(File.dirname(__FILE__)+"/server.rb", {
-              app_name: 'vk_daemon',
+Daemons.run_proc('vk_daemon', {
               dir_mode: :script,
               dir: 'daemon_data',
               backtrace: true,
               monitor: true,
               log_output: true
-            })
+            }) do
+  web=VK::IO::Web.new
+  VK::IO::Lan.new(web: web)
+  loop {}
+end
