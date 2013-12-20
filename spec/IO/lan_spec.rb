f17de611224bfac69c568ce422c1a7649167714c
Alexey Karasev <alex@homa-online.ru>
2013-12-20 12:33:38 +0000

    spec accounts for request order

--- spec/IO/lan_spec.rb
+++ spec/IO/lan_spec.rb
@@ -6,7 +6,7 @@ class VK
       before :all do
         @web=Object.new #doubles doesn't work in before all
         def @web.push(args={})
-          args[:respond_to].process_response("TestResponse")
+          args[:respond_to].process_response(args[:request]+"Response")
         end
         @lan=Lan.new(web: @web)
         Thread.abort_on_exception=true
@@ -23,16 +23,16 @@ class VK
       it "processes requests on localhost:9000" do
         @socket.puts "TestRequest1\neof"
         line=@socket.gets.chomp
-        line.should=="TestResponse"
+        line.should=="TestRequest1Response"
       end
 
       it "can process a bunch of requests through one socket" do
-        @socket.puts "TestRequest\nTestReqeust\neof"
+        @socket.puts "TestRequest1\nTestRequest2\neof"
         result=""
         2.times do
           result << @socket.gets.chomp
         end
-        result.should=="TestResponseTestResponse"
+        result.should=="TestRequest1ResponseTestRequest2Response"
       end
       
     end
