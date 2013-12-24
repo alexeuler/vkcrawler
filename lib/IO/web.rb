cf3d4db53d78a0436ae93c065e7b3a73be56f2ac
alex@laptop <karasev.alexey@gmail.com>
2013-12-25 00:49:11 +0400

    added Web

--- lib/IO/web.rb
+++ lib/IO/web.rb
@@ -1,8 +1,45 @@
 require 'rest_client'
 
-
 module Vk
   module IO
-    
+    class Web
+      def initialize(args={})
+        args=defaults.merge(args)
+        @responses=args[:responses]
+        @requests=args[:requests]
+        @frequency=args[:frequency]
+        @last_fetch=Time.now
+        start
+      end
+
+      def start
+        Thread.new do
+          while true
+            tuple=@requests.pop
+            delay
+            tuple.data=get(tuple.data)
+            @responses.push tuple
+          end
+        end
+      end
+
+      private
+
+      def get(request)
+        RestClient.get request
+      end
+
+      def delay
+        since_last_fetch=Time.now-@last_fetch # in seconds
+        pause=[(@frequency-since_last_fetch).round(4), 0].max
+        sleep(pause) unless pause==0
+        @last_fetch=Time.now
+      end
+
+      def defaults
+        {frequency: 0.2}
+      end
+
+    end
   end  
 end
---
+++ spec/IO/web_spec.rb
@@ -0,0 +1,31 @@
+require "IO/web"
+require "IO/tuple"
+require "thread"
+
+module Vk
+  module IO
+
+    describe "Web" do
+      
+      it "Reads from requests queue, fetches the request and pushes to responses queue with predetermined frequency" do
+        requests=Queue.new
+        responses=Queue.new
+        web=Web.new requests: requests, responses: responses, frequency: 0.2
+        web.should_receive(:get).exactly(5).times.and_return("Response")
+        
+        start=Time.now
+        5.times do |i|
+          requests.push Tuple.new(data: "Requests#{i}")
+        end
+        5.times do |i|
+          responses.pop.data.should=="Response"
+        end
+        run_time=Time.now - start
+        run_time.round(2).should==1
+        run_time.should>=1
+      end
+      
+    end
+
+  end
+end
