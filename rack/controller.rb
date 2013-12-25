a93d8a967cc1e57e44bfd216078b4ddd8b142a16
alex@laptop <karasev.alexey@gmail.com>
2013-12-26 00:36:49 +0400

    finished rack

--- rack/controller.rb
+++ rack/controller.rb
@@ -1,5 +1,7 @@
 require 'erb'
+require 'rest_client'
 require_relative "models/token"
+require 'json'
 
 class Controller
   def call(env)
@@ -7,10 +9,30 @@ class Controller
     @redirect_uri=ENV['VK_APP_URI']
     @id=ENV['VK_APP_ID']
     @secret=ENV['VK_APP_SECRET']
-    Rack::Response.new(render("index.html.erb"))  
+
+    request=Rack::Request.new(env)
+    if code=request.GET["code"]
+      resp=RestClient.get "https://api.vk.com/oauth/token?client_id=#{@id}&redirect_uri=#{@redirect_uri}&code=#{code}&client_secret=#{@secret}"
+      data=JSON.parse resp
+      if token=data["access_token"]
+        Token.delete_all
+        t=Token.new token: token, expires: Time.now+data["expires_in"]
+        t.save
+      end
+      response=Rack::Response.new
+      response.redirect("/")
+      response.finish
+    else
+      response=Rack::Response.new(render("index.html.erb"))  
+      response.finish
+    end
+    ActiveRecord::Base.clear_active_connections!
+    response
   end  
+
   def render(template)    
     path = File.expand_path("../views/#{template}", __FILE__)    
     ERB.new(File.read(path)).result(binding)  
   end
+
 end
--- rack/models/token.rb
+++ rack/models/token.rb
@@ -1,5 +1,2 @@
 class Token < ActiveRecord::Base
-  def time_left
-    created_at+expires-Time.now
-  end
 end
